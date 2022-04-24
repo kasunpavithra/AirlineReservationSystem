4f38c0d72e5b90cbfb12e116437fbc993b7db493
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2022-04-25 02:56:17 +0530

    I added login

---
+++ api/models/siteModel.js
@@ -0,0 +1,24 @@
+const db = require('../db/db');
+
+function getUsers(result) {
+    let sql = "select * from student";
+    db.query(sql, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        } else {
+            // console.log("tutorials: ", res);
+            result(null, res);
+        }
+    })
+}
+
+function loginUser(result,data) {
+    console.log(data);
+}
+
+module.exports = {
+  getUsers,
+  loginUser,
+};
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,63 @@
-const Login = () => {
-  return <h1>Login</h1>;
-};
+import { useState } from "react";
+import Axios from "axios";
+function Login() {
+  const [inputs, setInputs] = useState({});
+  const [loginStatus, setLoginStatus] = useState("");
 
+  const login = () => {
+    Axios.post("http://localhost:3001/api/login", {
+      name: inputs.username,
+      password: inputs.password,
+    }).then((response) => {
+      console.log(response);
+      if (!response) {
+        setLoginStatus("No user exists!!!");
+      } else if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].name);
+      }
+    });
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+    console.log(JSON.stringify(inputs));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // alert(JSON.stringify(inputs));
+    login();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={inputs.password || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <h1>{loginStatus}</h1>
+    </div>
+  );
+}
 export default Login;
