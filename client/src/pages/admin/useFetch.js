0ac6150191bb93bc833ccc404e37c7e2a48fb6f7
Pasan SK <pasanxavioz@gmail.com>
2022-04-27 01:11:20 +0530

    Implemented AllAuthorizedUser component

--- client/src/pages/admin/useFetch.js
+++ client/src/pages/admin/useFetch.js
@@ -6,32 +6,32 @@ const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
-    
+
         const abortCont = new AbortController();
 
-        fetch(url, {signal: abortCont.signal})
-        .then(res => {
-            if(!res.ok) throw Error("Could not fetch the data for that resource")
-            return res.json()
-        })
-        .then(data => {
-            setData(data)
-            setIsPending(false)
-            setError(null)
-        })
-        .catch(err => {
-            if(err.name === 'AbortError') {
-                console.log('fetch aborted')
-            } else {
+        fetch(url, { signal: abortCont.signal })
+            .then(res => {
+                if (!res.ok) throw Error("Could not fetch the data for that resource")
+                return res.json()
+            })
+            .then(data => {
+                setData(data)
                 setIsPending(false)
-            setError(err.message)
-            }
-        })
+                setError(null)
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('fetch aborted')
+                } else {
+                    setIsPending(false)
+                    setError(err.message)
+                }
+            })
 
-        return () => {abortCont.abort();}
+        return () => { abortCont.abort(); }
     }, [url])
 
-    return {data, isPending, error}
+    return { data, isPending, error }
 }
 
 export default useFetch;
