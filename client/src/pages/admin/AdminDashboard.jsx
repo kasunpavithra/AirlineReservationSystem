d6d6d32f4c34ee6a81378950f1b0705cf3f62507
Pasan SK <pasanxavioz@gmail.com>
2022-06-23 17:39:54 +0530

    resolved db.config issue

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -6,10 +6,20 @@ import './admin.css'
 import AllAuthorizedUsers from "./AllAuthorizedUsers";
 import UpdateRegisteredCustomer from "./UpdateRegisteredCustomer";
 import UpdateAuthorizedUser from "./UpdateAuthorizedUser";
+import AdminPanel from "./AdminPanel";
 
 const AdminDashboard = () => {
     return (
         <div className="container">
+            <Routes>
+                <Route path="/all-registered-customers" element={<AllRegisteredCustomers />} />
+                <Route path="/all-authorized-users" element={<AllAuthorizedUsers />} />
+                <Route path="/create-authorized-user" element={<CreateAuthorizedUser />} />
+                <Route path="/create-registered-customer" element={<CreateRegisteredCustomer />} />
+                <Route path="/update-registered-customer/:id" element={<UpdateRegisteredCustomer />} />
+                <Route path="/update-authorized-user/:id" element={<UpdateAuthorizedUser />} />
+                <Route path="/admin-panel" element={<AdminPanel />} />
+            </Routes>
 
             <h2 className="add-margin-top">Welcome Admin</h2>
 
@@ -18,16 +28,10 @@ const AdminDashboard = () => {
                 <li><NavLink to='/admin/all-authorized-users?filter=1' >all-authorized-users</NavLink></li>
                 <li><NavLink to='/admin/create-authorized-user' >create-authorized-user</NavLink></li>
                 <li><NavLink to='/admin/create-registered-customer' >create-registered-customer</NavLink></li>
+                <li><NavLink to='/admin/admin-panel' >admin-panel</NavLink></li>
             </ul>
 
-            <Routes>
-                <Route path="/all-registered-customers" element={<AllRegisteredCustomers />} />
-                <Route path="/all-authorized-users" element={<AllAuthorizedUsers />} />
-                <Route path="/create-authorized-user" element={<CreateAuthorizedUser />} />
-                <Route path="/create-registered-customer" element={<CreateRegisteredCustomer />} />
-                <Route path="/update-registered-customer/:id" element={<UpdateRegisteredCustomer />} />
-                <Route path="/update-authorized-user/:id" element={<UpdateAuthorizedUser />} />
-            </Routes>
+            
         </div>
     );
 }
---
+++ client/src/pages/admin/AdminPanel.jsx
@@ -0,0 +1,12 @@
+const AdminPanel = () => {
+    return (
+        <>
+            <h4>Admin nav</h4>
+
+            
+        </>
+
+    );
+}
+
+export default AdminPanel;
