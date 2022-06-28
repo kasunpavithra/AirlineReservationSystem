dbc5ab0d57a503e96bfe31c36779db1ded0edfe0
Kasun pavithra <mandalawattegekasunpavithra@gmail.com>
2022-06-28 18:57:57 +0530

    Role based authorization in frontend

--- client/src/utils/RequireAuth.jsx
+++ client/src/utils/RequireAuth.jsx
@@ -1,23 +1,33 @@
 import { useAuth } from "./auth";
-import { Navigate, useLocation } from "react-router-dom";
-export const RequireAuth = ({ children,allowedRoles }) => {
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+// export const RequireAuth = ({ children, allowedRoles }) => {
+//   const location = useLocation();
+
+//   try {
+//     // var user=jwtDecode(Token.getAccessToken())
+//   } catch (err) {
+//     var user = null;
+//   }
+
+//   return allowedRoles?.find((role) => user?.role?.includes(role)) ? (
+//     children
+//   ) : user ? (
+//     <Navigate to="/unauthorized" state={{ from: location.pathname }} replace />
+//   ) : (
+//     <Navigate to="/login" state={{ from: location.pathname }} replace />
+//   );
+// };
+
+export const RequireAuth = ({ allowedRoles }) => {
+  const { auth } = useAuth();
   const location = useLocation();
 
-  try{
-    // var user=jwtDecode(Token.getAccessToken())
-   }
-   catch(err){
-    var user=null
-   } 
-  
-  return (
-    allowedRoles?.find(role => user?.role?.includes(role))? children
-        : user? <Navigate to="/unauthorized" state={{ from: location.pathname }} replace />
-            : <Navigate to="/login" state={{ from: location.pathname }} replace />
-);
+  return auth?.role && allowedRoles.includes(auth.role) ? (
+    <Outlet />
+  ) : auth?.user ? (
+    <Navigate to="/unauthorized" state={{ from: location }} replace />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
-
-
-
-
- 
